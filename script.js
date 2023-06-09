6a66fb8be28efad8a93c36f49c27f3e9d9a46c47
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-06-09 17:20:05 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.querySelector("body").appendChild("h2");
+h2.textContent = "moringa school will develop your codding life and make you understand more about code challenges";
+<script src="script.js"></script>
